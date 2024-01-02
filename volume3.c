8fefb34402135f07aa0885221cf42afbd4ea9db1
TanishaHirpara <150216881+TanishaHirpara@users.noreply.github.com>
2024-01-02 10:36:22 +0530

    Add files via upload

---
+++ volume3.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+enum student{
+
+  monday, tuesday, wednesday, thursday, friday, saturday, sunday
+  };
+
+main()
+
+{
+	enum student m;
+	
+	printf("%d\n" ,monday);
+	printf("%d\n" ,tuesday);
+	printf("%d\n" ,wednesday);
+	printf("%d\n" ,thursday);
+	printf("%d\n" ,friday);
+	printf("%d\n" ,saturday);
+	printf("%d\n" ,sunday);
+	
+}
